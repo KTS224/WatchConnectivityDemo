b8cdc6bd2307df85970ef09081d7d68f25e2a831
KTS224 <ts4683@gmail.com>
2024-04-16 05:04:05 +0900

    워치 심박수 애니메이션 추가

--- watchConDemo/connectDemo Watch App/Views/ContentView.swift
+++ watchConDemo/connectDemo Watch App/Views/ContentView.swift
@@ -9,33 +9,139 @@ import Foundation
 import HealthKit
 import SwiftUI
 
+struct HeartParticle: Identifiable {
+    var id: UUID = .init()
+}
+
+struct PulseHeartView: View {
+    @State private var startAnimation = false
+    
+    var body: some View {
+        Image(systemName: "brain.head.profile")
+            .font(.largeTitle)
+            .foregroundStyle(.green)
+            .scaleEffect(startAnimation ? 4 : 1)
+            .opacity(startAnimation ? 0 : 0.5)
+            .onAppear {
+                withAnimation(.linear(duration: 3)) {
+                    startAnimation = true
+                }
+            }
+    }
+}
+
 struct ContentView: View {
     @StateObject private var healthKitManager = HealthKitManager()
     var model = WatchConnectivityProvider()
     @ObservedObject var myTimer = MyTimer()
     @State private var timer: Timer?
 
+    // 애니메이션용 변수
+    @State private var beatAnimation: Bool = false
+    @State private var showPulses: Bool = false
+    @State private var pulsedHearts: [HeartParticle] = []
+    
     var body: some View {
-//        Button(action: {
-//            healthKitManager.stopWorkout()
-//            
-//            timer?.invalidate()
-//            timer = nil
-//            
-//        }, label: {
-//            Text("운동중단")
-//        })
+        
+        VStack {
+            ZStack {
+                
+                if showPulses {
+                    TimelineView(.animation(minimumInterval: 1.5, paused: false)) { timeline in
+                        Canvas { context, size in
+                            for heart in pulsedHearts {
+                                if let resolvedCiew = context.resolveSymbol(id: heart.id) {
+                                    let centerX = size.width / 2
+                                    let centerY = size.height / 2
+                                    
+                                    context.draw(resolvedCiew, at: CGPoint(x: centerX, y: centerY))
+                                }
+                            }
+                            
+                        } symbols: {
+                            ForEach(pulsedHearts) {
+                                PulseHeartView()
+                                    .id($0.id)
+                            }
+                        }
+                        .onChange(of: timeline.date) { oldValue, newValue in
+                            let pulsedHeart = HeartParticle()
+                            pulsedHearts.append(pulsedHeart)
+                            
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                                pulsedHearts.removeAll(where: { $0.id == pulsedHeart.id })
+                            }
+                        }
+                    }
+                } else {
+                    TimelineView(.animation(minimumInterval: 1.5, paused: false)) { timeline in
+                        Canvas { context, size in
+                            for heart in pulsedHearts {
+                                if let resolvedCiew = context.resolveSymbol(id: heart.id) {
+                                    let centerX = size.width / 2
+                                    let centerY = size.height / 2
+                                    
+                                    context.draw(resolvedCiew, at: CGPoint(x: centerX, y: centerY))
+                                }
+                            }
+                            
+                        } symbols: {
+                            ForEach(pulsedHearts) {
+                                PulseHeartView()
+                                    .id($0.id)
+                            }
+                        }
+                    }
+                }
+                
+                
+                Image(systemName: "brain.head.profile")
+                    .font(.title)
+                    .foregroundStyle(.green.gradient).opacity(0.7)
+                    .symbolEffect(.bounce, options: !beatAnimation ? .default : .repeating.speed(1), value: beatAnimation)
+            }
+            .overlay(alignment: .bottomLeading) {
+                VStack(alignment: .leading, spacing: 5, content: {
+                    Text(showPulses ? "공부중" : "멈춤")
+                        .font(.title3.bold())
+                        .foregroundStyle(showPulses ? .white : .gray)
+                    
+                    HStack(alignment: .bottom, spacing: 6, content: {
+                        Text(showPulses ? "\(healthKitManager.heartRate)" : "...")
+                            .font(.system(size: 45))
+                            .contentTransition(.numericText(value: Double(88)))
+                            .foregroundStyle(.white)
+                        
+                        Text("BPM")
+                            .foregroundStyle(.red.gradient)
+                    })
+                    
+                    Text(showPulses ? "평균 \(healthKitManager.heartRate-6) BPM" : "...")
+                        .font(.caption2)
+                        .foregroundStyle(.gray)
+                })
+            }
+        }
+        
+        // MARK: - 측정하기 / 측정중 버튼
         Button(action: {
             model.sendButtonPressed()
             healthKitManager.startWorkout()
+            showPulses.toggle()
         }) {
             Text(model.buttonText)
         }
         .disabled(model.buttonDisabled)
         
+        // MARK: -
+        
         if model.buttonDisabled {
             VStack {
-                Text("Heart Rate: \(healthKitManager.heartRate) bpm")
+//                Text("Heart Rate: \(healthKitManager.heartRate) bpm")
+                HStack {
+                    Rectangle()
+                        .frame(width: 0, height: 0)
+                }
             }
             .onAppear {
                 print("ONAPPEAR")
@@ -52,16 +158,21 @@ struct ContentView: View {
                     print(Int(healthKitManager.heartRate))
                 }
             }
+            .onDisappear {
+                showPulses.toggle()
+            }
         } else {
-            Rectangle()
-                .frame(width: 5, height: 5)
-                .foregroundStyle(.cyan)
-                .onAppear {
-                    healthKitManager.stopWorkout()
-                    
-                    timer?.invalidate()
-                    timer = nil
-                }
+            HStack {
+                Rectangle()
+                    .frame(width: 0, height: 0)
+            }
+            .onAppear {
+                print("onappear healthKitManager.stopWorkout()")
+                healthKitManager.stopWorkout()
+                
+                timer?.invalidate()
+                timer = nil
+            }
         }
         
     }
